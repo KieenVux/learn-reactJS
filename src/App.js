7a724dfebeaac4e1d6d15b99e8ec49ae3c64b74e
KieenVux <40339436+KieenVux@users.noreply.github.com>
2021-09-03 11:35:26 +0700

    update

--- src/App.js
+++ src/App.js
@@ -6,19 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <br />
-        <h1>Kien Vu 2110</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Kien Vu 2110</p>
       </header>
     </div>
   );
