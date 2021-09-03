702c1f34612254d6e4368ae5b2d9e738a20c84d6
KieenVux <40339436+KieenVux@users.noreply.github.com>
2021-09-03 11:31:50 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Kien Vu 2110
+          Edit <code>src/App.js</code> and save to reload.
+          <br />
+          Kien Vu 2110
         </p>
         <a
           className="App-link"
